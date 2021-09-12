653f9d43cc4df8923a84fad0d7035b23d078055f
Bartolome Chavez <bchavez@technisys.com>
2021-09-12 15:11:49 -0300

    counter

---
+++ src/app/CONFIGS.ts
@@ -0,0 +1,3 @@
+export enum CONFIGS {
+  END_COUNTDOWN_COUNTER = "2021-09-27"
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountdownConfig } from 'ngx-countdown';
+import { CONFIGS } from './CONFIGS';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'magenta-website';
+
+  day_config: CountdownConfig = {
+    stopTime: new Date(CONFIGS.END_COUNTDOWN_COUNTER).getTime(),
+    format: "dd"
+  };
+
+  hours_config: CountdownConfig = {
+    stopTime: new Date(CONFIGS.END_COUNTDOWN_COUNTER).getTime(),
+    format: "HH"
+  };
+
+  minutes_config: CountdownConfig = {
+    stopTime: new Date(CONFIGS.END_COUNTDOWN_COUNTER).getTime(),
+    format: "mm"
+  };
+
+  handleEvent(params) {
+    console.log(params);
+
+  }
+
 }
